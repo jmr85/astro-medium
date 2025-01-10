37789de78e684c42b184b5a3cddb2bedab9d1856
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-01-09 23:17:42 -0300

    feat: add Vercel ISR for automatic Medium posts updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	adapter: vercel({
+		imageService: true,
+		webAnalytics: {
+			enabled: true,
+		},
+		isr: {
+			expiration: 3600,
+		},
+	}),
 });
